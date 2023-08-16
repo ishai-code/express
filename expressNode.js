a478bf5902bbd7738b44a9a9a0985696d0aeb14a
Isaschar Madar <pryysy88@gmail.com>
2023-08-16 01:37:37 -0700

    exercise 3 and 4

--- expressNode.js
+++ expressNode.js
@@ -1,7 +1,10 @@
 const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcrypt");
 express = require("express");
 const app = express();
 const port = 8080;
+const saltRounds = 10;
+
 app.use(express.json());
 
 function createId() {
@@ -13,17 +16,27 @@ id2 = "639383bd-3dc5-46d1-9121-27fb6ac5a61d";
 id3 = "38873ed8-c13a-441f-9d8c-2987ece26b7b";
 
 const users = [
-  { id: id1, name: "shay", pass: 4637 },
-  { id: id2, name: "dan", pass: 5687 },
-  { id: id3, name: "elad", pass: 8743 },
+  { id: id1, name: "shay", pass: "1234" },
+  { id: id2, name: "dan", pass: "5687" },
+  { id: id3, name: "elad", pass: "8743" },
 ];
 
-app.get("/", (req, res) => {
-  res.send(users);
+function hashPass(pass) {
+  let hash = bcrypt.hashSync(pass, saltRounds);
+  return hash;
+}
+
+users.forEach((element) => {
+  element.pass = hashPass(element.pass);
+  console.log(element.pass);
 });
 
 app.listen(port, () => {
   console.log(`port:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send(users);
 });
 
 app.get("/users/:id", (req, res) => {
@@ -40,7 +53,8 @@ app.get("/users/:id", (req, res) => {
 });
 
 app.post("/users", (req, res) => {
-  const newUser = { id: createId(), name: req.body.name, pass: req.body.pass };
+  const password = hashPass(req.body.pass);
+  const newUser = { id: createId(), name: req.body.name, pass: password };
   users.push(newUser);
   res.send(users);
 });
@@ -74,6 +88,23 @@ app.delete("/users/:id", (req, res) => {
     res.send("not exist such a user in our data");
   }
 });
+
+
+app.post("/users", (req, res) => {
+  const userName = req.body.name;
+  const userPass = req.body.pass;
+  let bool = 0;
+  users.forEach((element) => {
+    if (element.name == userName & bcrypt.compareSync(userPass, element.pass)) {
+      res.send("user is connected");
+      bool++;
+    }
+  });
+  if (!bool) {
+    res.send("not exist such a user in our data");
+  }
+});
+
 
 
 
